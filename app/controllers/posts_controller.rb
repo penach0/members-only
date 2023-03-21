dd63393dfc8782a49f47a61827eb13ba228275db
penach0 <josemanuel.penacho@gmail.com>
2023-03-21 08:36:48 +0000

    Create form for new Post
    - Add a basic stylesheet for a cleaner display
    - Added empty require_login method to Posts controller just to avoid
    errors for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
 class PostsController < ApplicationController
   before_action :require_login, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  private
+    def require_login
+      
+    end
 end
