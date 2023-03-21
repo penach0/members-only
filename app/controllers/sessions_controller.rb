42ac6a81d99c2406bdd3ac94d1d793abeb0429e4
penach0 <josemanuel.penacho@gmail.com>
2023-03-21 15:54:06 +0000

    Create simple #create action for Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
-  
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user.authenticate?(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Your e-mail or password are incorrect"
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
