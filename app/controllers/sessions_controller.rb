f2876e4ba1b7d99c6630b18dfaedd829641ff0a6
penach0 <josemanuel.penacho@gmail.com>
2023-03-21 16:09:24 +0000

    Create #destroy action for Sessions
    - Change the top info ("Sign in or sign up") to an appropriate message
    and a possibility of logging out if there is a user already logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
