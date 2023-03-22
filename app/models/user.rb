e67d4d5729e4cb727aeee643149ea53b5e402e62
penach0 <josemanuel.penacho@gmail.com>
2023-03-22 07:58:02 +0000

    Rename password column to password_digest
    - Starting to implement a more robust authentication
    - Deleted all User records since passwords wouldn't work - for this
    I had to delete their dependent posts also

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :username, :email, :password, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_130641) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_074744) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_130641) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
